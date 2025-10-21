61b8cb89e221458dc292bec8512f0e3382196147
matheuslima084 <ms335102@gmail.com>
2025-10-21 14:31:39 -0300

    editar contato

--- core/models.py
+++ core/models.py
@@ -4,5 +4,8 @@ class Contato(models.Model):
     nome = models.CharField(max_length=20)
     sobrenome = models.CharField(max_length=20)
     descricao = models.TextField(max_length=255)
-    telefone = models.IntegerField()
+    telefone = models.CharField(max_length=20)  # Melhor usar CharField para telefone
     imagem = models.ImageField(upload_to='core_imagens/%Y/%m/', blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.nome} {self.sobrenome}'
--- core/views.py
+++ core/views.py
@@ -1,35 +1,33 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib import messages
-from django.utils.translation.template import context_re
 from core.models import Contato
 
 def add_contato(request):
-    if request.method != 'POST':
-        return render(request, 'core/add_contato.html')
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        sobrenome = request.POST.get('sobrenome')
+        descricao = request.POST.get('descricao')
+        telefone = request.POST.get('telefone')
+        imagem = request.FILES.get('imagem')
 
-    nome = request.POST.get('nome')
-    sobrenome = request.POST.get('sobrenome')
-    descricao = request.POST.get('descricao')
-    telefone = request.POST.get('telefone')
-    imagem = request.FILES.get('imagem')  # Corrigido
+        if not nome or not sobrenome:
+            # Poderia passar uma mensagem de erro aqui, se quiser
+            return render(request, 'core/add_contato.html')
 
-    if not nome or not sobrenome:
-        return render(request, 'core/add_contato.html')
-
-    salvarContato = Contato.objects.create(
-        nome=nome,
-        sobrenome=sobrenome,
-        descricao=descricao,
-        telefone=telefone,
-        imagem=imagem
-    )
+        Contato.objects.create(
+            nome=nome,
+            sobrenome=sobrenome,
+            descricao=descricao,
+            telefone=telefone,
+            imagem=imagem
+        )
+        return redirect('listar_contato')  # Redireciona após criar para a lista
 
     return render(request, 'core/add_contato.html')
 
 
 def listar_contato(request):
-    contato = Contato.objects.all()
-    return render(request, 'core/listar_contato.html', {'contato': contato})
+    contatos = Contato.objects.all()
+    return render(request, 'core/listar_contato.html', {'contato': contatos})
 
 
 def editarContato(request, id):
@@ -48,10 +46,10 @@ def editarContato(request, id):
         contato.save()
         return redirect('listar_contato')
 
-    return render(request, 'core/editar_contato.html', {'contato': contato})  # Corrigido o nome do template
+    return render(request, 'core/editar_contato.html', {'contato': contato})
 
 
 def excluirContato(request, id):
-    contato = get_object_or_404(Contato, id=id)  # mais seguro
+    contato = get_object_or_404(Contato, id=id)
     contato.delete()
     return redirect('listar_contato')
